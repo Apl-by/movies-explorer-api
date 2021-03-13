72557aea93c18b6092dd45c747b756bb1a62eb2d
Aliaksandr Lahunou <mymail15-87y@yandex.ru>
2021-03-13 13:49:16 +0300

    fix:fix deleteMovie controller

--- controllers/movies.js
+++ controllers/movies.js
@@ -1,4 +1,5 @@
 const Movie = require('../models/movie');
+const ForbiddenError = require('../errors/ForbiddenError');
 
 const createMovies = (req, res, next) => {
   Movie.create({ ...req.body, owner: req.user._id })
@@ -13,8 +14,14 @@ const getMovies = (req, res, next) => {
 };
 
 const deleteMovie = (req, res, next) => {
-  Movie.findOneAndDelete({ _id: req.params._id }).orFail()
-    .then(() => res.send({ message: 'Фильм удален из сохранённых' }))
+  Movie.findById(req.params._id).orFail()
+    .then((movie) => {
+      if (String(movie.owner) !== req.user._id) {
+        return next(new ForbiddenError('Нельзя удалить чужую карточку с фильмом'));
+      }
+      return Movie.findByIdAndRemove(req.params._id).orFail()
+        .then(() => res.send({ message: 'Карточка с фильмом удалена из сохранённых' }));
+    })
     .catch(next);
 };
 
